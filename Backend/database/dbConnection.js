40c5ccfc52523c85d47a339366e00efe05605f83
Harsh18122001 <harshpoddar76@gmail.com>
2025-04-06 04:14:59 +0530

    first commit

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "Life_Care_Hospital"
+    }).then(() => {
+        console.log("Connected to Database");
+    }).catch(err => {
+        console.log(`Some error occured while connecting to Database: ${err}`);
+    })
+}
