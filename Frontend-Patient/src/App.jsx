0d747b6705e1e6465e9afd88193dc8d409cfb9f8
Harsh Poddar <97839990+Harsh18122001@users.noreply.github.com>
2025-04-06 05:22:56 +0530

    Update App.jsx

--- Frontend-Patient/src/App.jsx
+++ Frontend-Patient/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
+          "https://life-care-q5tc.onrender.com/api/v1/user/patient/me",
           { withCredentials: true }
         );
         setIsAuthenticated(true);
