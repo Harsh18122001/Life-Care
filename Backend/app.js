248d8df72fceda444faf855cc957a6e7ef8f6258
Harsh Poddar <97839990+Harsh18122001@users.noreply.github.com>
2025-04-06 13:40:08 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 config({ path: "./config/config.env" })
 app.use(cors(
     {
-        origin: ["http://localhost:5174" ,"http://localhost:5173"],
+        origin: ["https://life-care-fe-patient.onrender.com" ,"http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
